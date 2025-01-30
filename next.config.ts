e70ebac0eef513c01f277eb71469bd1c1b1de2db
Jay <74604447+jasonprogrammer775@users.noreply.github.com>
2025-01-30 08:41:52 +0100

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental:{
+    dynamicIO: true,
+    authInterrup
+
+  }
+};
+
+export default nextConfig;
